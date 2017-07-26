dfea5510bb52a3b7b5b14ee20f5509a785b263ce
Glen Young <extractofcactus@icloud.com>
2017-07-26 16:23:39 +0100

    concat test passed

---
+++ array_tasks.js
@@ -0,0 +1,44 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		var newArray = []
+		for (element of arr1) {
+			newArray.push(element);
+		}
+		for (element of arr2) {
+			newArray.push(element);
+		}
+		return newArray;
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+		
+	// },
+
+	// square: function (arr) {
+		
+	// },
+
+	// sum: function (arr) {
+		
+	// },
+
+	// findDuplicates: function (arr) {
+		
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+		
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+		
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+		
+	// }
+
+}
+
+module.exports = arrayTasks
